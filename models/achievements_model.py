54c250ce2eb352ab6d54ef219062979b0601ff16
Oliver <Oliver@spyrakis.se>
2025-03-25 15:32:18 +0100

    Added user models (Hela gruppen)

---
+++ models/achievements_model.py
@@ -0,0 +1,9 @@
+from config.db_config import db
+from sqlalchemy.sql import func
+
+class Achievements(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    achievement_name = db.Column(db.String(50), nullable=False)
+    achievement_date = db.Column(db.DateTime, nullable=False, default=func.now())
