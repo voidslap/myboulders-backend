59073a0b63ef6d8a738247cef9242e2eb19add4b
Chaincler <73599394+voidslap@users.noreply.github.com>
2025-03-26 13:55:26 +0100

    model update based on new er-diagram

--- models/difficulty_levels_model.py
+++ models/difficulty_levels_model.py
@@ -1,8 +1,10 @@
 from config.db_config import db
-from sqlalchemy.sql import func
 
 class DifficultyLevel(db.Model):
     __tablename__ = 'difficulty_levels'
+    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    grade = db.Column(db.String(50), nullable=False)
-    # routes relationship comes from backref in Route model
+    grade = db.Column(db.String(250), nullable=False)
+    
+    def __repr__(self):
+        return f"<DifficultyLevel {self.id}: {self.grade}>"
