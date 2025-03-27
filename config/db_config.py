efb714a6ce27904efbaf73a0e8bc7599cac9f48d
jorpanp <johannesp02@gmail.com>
2025-03-27 15:38:17 +0100

    Created routes for /login and /logout aswell as finished the auth-controller. Added a config class to load SECRET_KEY from enviroment

--- config/db_config.py
+++ config/db_config.py
@@ -5,5 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 db = SQLAlchemy()
 
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
 def get_db_uri():
     return os.getenv('DATABASE_URL')
--- routes/auth_routes..py
+++ routes/auth_routes..py
@@ -1,22 +1,32 @@
-from flask import Blueprint, jsonify, request
-from controllers.auth_controller import authenticate_user
-
+from flask import Blueprint, jsonify, request, make_response
+from controllers import auth_controller
 
 auth_routes = Blueprint('auth_routes', __name__)
 
+#Login
 @auth_routes.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
 
-    if not data or 'username' not in data or 'password' not in data:
+    if not data or not data.get('username') or not data.get('password'):
         return jsonify({'error': 'Missing username or password'}), 400
-    
 
-    user_data, error = authenticate_user(data['username'], data['password'])
+    token, error = auth_controller.authenticate_user(
+        data['username'],
+        data['password']
+    )
 
     if error:
         return jsonify({'error': error}), 401
-    
-    return jsonify({'message': 'Login successful {}'.format(user_data['username'])}), 200
 
-    
+    response = make_response(jsonify({'message': 'Login successful'}))
+    response.set_cookie('token', token, httponly=True)
+
+    return response, 200
+
+#Logout (deletes JWT-Cookie)
+@auth_routes.route('/logout', methods=['POST'])
+def logout():
+    response = make_response(jsonify({'message': 'Logged out'}))
+    response.set_cookie('token', '', expires=0)
+    return response, 200
