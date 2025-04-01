c0be01c8d3d471b70b45bd569f494afa69b4136f
Chaincler <73599394+voidslap@users.noreply.github.com>
2025-04-01 13:40:23 +0200

    Fixed issues with imports. We can now run app.py without crashing

--- routes/leaderboard.py
+++ routes/leaderboard.py
@@ -3,6 +3,9 @@ from controllers.leaderboard_controller import get_leaderboard_data
 
 leaderboard_bp = Blueprint('leaderboard', __name__)
 
+
+
+
 @leaderboard_bp.route('/leaderboard', methods=['GET'])
 def leaderboard():
     data = get_leaderboard_data()
