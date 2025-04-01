8a440d8ecf9c2bb8215489ee1809169f57e6a862
Chaincler <73599394+voidslap@users.noreply.github.com>
2025-04-01 14:59:46 +0200

    Created journal_controller.py and journal_routes.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from routes.user_routes import user_routes
 from routes.auth_routes import auth_routes
+from routes.leaderboard import leaderboard_bp
+from routes.journal_routes import journal_routes
 from config.db_config import db, get_db_uri
 
 app = Flask(__name__)
@@ -18,6 +20,8 @@ db.init_app(app)
 # Register blueprints
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(leaderboard_bp, url_prefix='/api')
+app.register_blueprint(journal_routes, url_prefix='/api/journal')
 
 @app.route('/')
 def hello_world():
