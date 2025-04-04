e492737938d75c44925d9497f3559d8688848542
voidslap <charlie.ulriksson99@gmail.com>
2025-04-04 16:41:13 +0200

    Added testing for auth routes

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+import os
+import sys
+
+# Add the parent directory to sys.path to ensure imports work correctly
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
