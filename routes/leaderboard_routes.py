77dbf309aceacccaa1adbadb540898359258a833
jorpanp <johannesp02@gmail.com>
2025-04-04 20:39:18 +0200

    Added route to remove user from leaderboard

--- routes/leaderboard_routes.py
+++ routes/leaderboard_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
 from controllers.leaderboard_controller import get_leaderboard_data
+from models.users_model import User
+from config.db_config import db
 
 leaderboard_routes = Blueprint('leaderboard', __name__)
 
@@ -10,3 +12,14 @@ leaderboard_routes = Blueprint('leaderboard', __name__)
 def leaderboard():
     data = get_leaderboard_data()
     return jsonify(data)
+
+@leaderboard_routes.route('/<int:user_id>', methods=['DELETE'])
+def delete_user_from_leaderboard(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    
+    # Remove user and their associated data
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": "User removed from leaderboard"}), 200
